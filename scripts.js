37a6f203e9b18f4815926a8b363560771329d916
Ogareg <ogareg.miniatures@gmail.com>
2024-06-16 22:01:03 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ function initMap() {
         container: 'map',
         style: 'mapbox://styles/mapbox/dark-v10',
         center: [11.91878, 57.69357],
-        zoom: 13
+        zoom: 12
     });
 
     // Function to parse CSV and add markers with popups to the map
