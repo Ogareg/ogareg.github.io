4cb2d90723ab7837c65b2a24844fe5aa5575bfde
Ogareg <ogareg.miniatures@gmail.com>
2024-06-22 20:38:04 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -54,7 +54,7 @@ function initMap() {
                 var popupContent = `<h3>${name}</h3><br>${stad}<br>${adress}<br><a href="${hemsida}" target="_blank">${hemsida}</a>`;
 
                 // Create popup
-                var popup = new mapboxgl.Popup({ offset: 25 })
+                var popup = new mapboxgl.Popup({ offset: 0 })
                     .setHTML(popupContent);
 
                 // Bind popup to marker
